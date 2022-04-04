ac5f38ac6d0df348d715ce5c3d4b58b9222d6cd2
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-04-04 16:49:47 +0000

    Challenge set up

--- main.rb
+++ main.rb
@@ -1,1 +1,28 @@
-puts "Hello, World!"
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sansfransicso" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+
+#Get city names from hash 
+def get_city_names(somehash)
+
+end
+
+#get area code based on given hash and key
+def get_area_code(somehash, key)
+
+end
+
+#Execution flow
+loop do
+
+end 
+
